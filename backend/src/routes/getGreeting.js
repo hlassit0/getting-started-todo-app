94fde963254bb75bdf130f516ee36d48e500d826
hlassit0 <lassitermheather@gmail.com>
2026-04-21 18:55:32 -0400

    Fix backend formatting

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,7 @@
 const GREETINGS = [
-  "Whalecome!",
-  "All hands on deck!",
-  "Charting the course ahead!",
+  'Whalecome!',
+  'All hands on deck!',
+  'Charting the course ahead!',
 ];
 
 module.exports = async (req, res) => {
